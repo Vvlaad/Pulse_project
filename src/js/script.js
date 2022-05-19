90beb0f423efb2929b820aa6f334545183aceee8
Vlad <EidanPraid@protonmail.com>
2022-05-19 13:42:09 +0300

    four sections done

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }
+        ]
+      });
+  });
